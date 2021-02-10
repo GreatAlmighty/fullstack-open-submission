d59059705bca257790f18ebf50cee7ae83c1b738
kewal <kewal@think201.com>
2021-02-10 20:58:14 +0530

    Event handlers with returning functions

--- part1/src/index.js
+++ part1/src/index.js
@@ -98,7 +98,7 @@ const Button = (props) => {
 
 }
 
-const History = (props) => {
+const History = props => {
 
   const { allClicks } = props;
 
@@ -117,11 +117,22 @@ const History = (props) => {
   )
 }
 
+const Display = props => {
+  const {value} = props;
+  return (
+    <div>{value}</div>
+  )
+}
+
 const App = () => {
 
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => setValue(newValue)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
@@ -147,10 +158,22 @@ const App = () => {
       {left}
       {/* <button onClick={handleLeftClick}>left</button> */}
       <Button handleClick={handleLeftClick} text={'left'} />
-      {/* <Button handleClick={setToZero} text={'zero'} /> */}
-      <Button handleClick={handleRightClick} text={'right'} />
+      {/* <Button handleClick={() => setAll(allClicks.concat(' '))} text={'zero'} /> */}
+      <Button handleClick={(handleRightClick)} text={'right'} />
       {/* <button onClick={handleRightClick}>right</button> */}
       {right}
+      {/* <button onClick={hello('world')}>for world</button>
+      <button onClick={hello('react')}>for react</button>
+      <button onClick={hello('function')}>for function</button> */}
+      {/* <button onClick={() => setToValue(1000)}>thousand</button> */}
+      {/* <button onClick={() => setToValue(0)}>reset</button> */}
+      {/* <button onClick={() => setToValue(value + 1)}>increment</button> */}
+
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+
+      <Display value={value} />
       <History allClicks={allClicks} />
     </>
   )
